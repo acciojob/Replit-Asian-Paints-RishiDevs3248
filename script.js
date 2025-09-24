826b9a7f436eeb7dcb0616c697676b9b6818b80e
Hrishikesh Alabnur <144805491+RishiDevs3248@users.noreply.github.com>
2025-09-24 10:47:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+let items = document.querySelectorAll('.grid-item')
+
+let changebutton = document.getElementById('change_button')
+let reset = document.getElementById('Reset')
+
+changebutton.addEventListener("click", () => {
+	items.forEach((cell)=>{
+		cell.style.backgroundColor='transparent'
+	})
+	let ipid = document.getElementById('block-id').value
+	let ipco = document.getElementById('colour-id').value
+
+	let changeCell = document.getElementById(`${ipid}`)
+	changeCell.style.backgroundColor=ipco
+})
+
+reset.addEventListener("click", () => {
+	items.forEach((cell)=>{
+		cell.style.backgroundColor='transparent'
+	})
+})
